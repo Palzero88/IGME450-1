a23ff375faef8fc03b1b5cfde35b4e14ee4aa6dd
jcp3213 <42848748+jcp3213@users.noreply.github.com>
2020-09-17 15:20:19 -0400

    Implemented jumping and wrapping platforms that ramp up their speed over time.

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+
+    private Rigidbody2D rigidbody2d;
+    private float platformSpeed = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = transform.position + new Vector3(-platformSpeed, 0.0f, 0.0f);
+
+        if (transform.position.x <= -42)
+        {
+
+            transform.position = new Vector3(42, transform.position.y, transform.position.z);
+
+        }
+
+        platformSpeed += 0.01f * Time.deltaTime;
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private BoxCollider2D playerBoxCollider;
+    //private GameObject player;
+    private Rigidbody2D rigidbody2d;
+    [SerializeField] private LayerMask platformsLayerMask;
+    private float jumpSpeed = 10.0f;
+    private float movementSpeed = 5.0f;
+    private bool hasHitZenith = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //player = player.GetComponent<GameObject>();
+        playerBoxCollider = transform.GetComponent<BoxCollider2D>();
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.W) && IsGrounded())
+        {
+
+            rigidbody2d.velocity = Vector2.up * jumpSpeed * Time.deltaTime * 20;
+            hasHitZenith = false;
+
+        }
+        else if (Input.GetKey(KeyCode.W) && !hasHitZenith)
+        {
+
+            rigidbody2d.velocity += Vector2.up * jumpSpeed * Time.deltaTime * 2;
+
+        }
+
+        if (rigidbody2d.velocity.y >= 7.5f || Input.GetKeyUp(KeyCode.W))
+        {
+
+            hasHitZenith = true;
+
+        }
+
+    }
+
+    private bool IsGrounded()
+    {
+
+        RaycastHit2D groundedCheck = Physics2D.BoxCast(playerBoxCollider.bounds.center, playerBoxCollider.bounds.size, 0.0f, Vector2.down, 0.01f, platformsLayerMask);
+
+        return groundedCheck.collider != null;
+
+    }
+
+}
