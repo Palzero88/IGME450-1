5136bb1e38f772d7c6b5bd2e0903d7ad16b5d960
Palzero88 <obp4206@rit.edu>
2020-09-27 15:15:43 -0400

    some platform work. Having the jumps first will help me work it out a bit better

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,12 @@ public class GameManager : MonoBehaviour
     public float platformSpeed = 0.05f;
     private float counter = 2.0f;
 
+    //regular platform var
+    public float platformMinRange = -3.0f;
+    public float platformMaxRange = 3.0f;
+    public float platformMinWidth = 50.0f;
+    public float platformMaxWidth = 100.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,18 +28,28 @@ public class GameManager : MonoBehaviour
         counter += Time.deltaTime;
 
         Debug.Log(counter);
-            
-        if (platform.transform.position.x <= 0.0f)
+
+        if (platform.transform.position.y <= -2.8f)
+        {
+            counter = 0.0f;
+
+            //high platform spawns
+            platform = Instantiate(platform, new Vector3(15.0f, 3.0f, 1.0f), Quaternion.identity);
+            platform.transform.localScale = new Vector3(Random.Range(platformMinWidth, platformMaxWidth), 3.5f, 1.0f);
+
+        }
+        else if (platform.transform.position.x <= 0.0f)
         {
 
             counter = 0.0f;
 
-            platform = Instantiate(platform, new Vector3(15.0f, Random.Range(-4.748f, 1.0f), -5.25f), Quaternion.identity);
-            platform.transform.localScale = new Vector3(Random.Range(35.0f, 100.0f), 3.5f, 1.0f);
+            //regular platform spawns
+            platform = Instantiate(platform, new Vector3(15.0f, Random.Range(platformMinRange, platformMaxRange), 1.0f), Quaternion.identity);
+            platform.transform.localScale = new Vector3(Random.Range(platformMinWidth, platformMaxWidth), 3.5f, 1.0f);
 
         }
 
-        platformSpeed += 0.01f * Time.deltaTime;
+        platformSpeed += 0.001f * Time.deltaTime;
 
     }
 
